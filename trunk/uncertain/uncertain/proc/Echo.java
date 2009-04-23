5b51c054f425536b1a2856d651fd7ff485208cd3
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2009-04-23 13:24:57 +0000

    new logging architecture

--- trunk/uncertain/uncertain/proc/Echo.java
+++ trunk/uncertain/uncertain/proc/Echo.java
@@ -5,6 +5,9 @@ package uncertain.proc;
 import java.util.logging.Logger;
 
 import uncertain.composite.TextParser;
+import uncertain.event.RuntimeContext;
+import uncertain.logging.ILogger;
+import uncertain.logging.LoggingContext;
 
 public class Echo extends AbstractEntry {
     
@@ -18,10 +21,17 @@ public class Echo extends AbstractEntry {
         String m = null;
         if(Message==null) m = runner.getContext().toXML();
         else m = TextParser.parse( Message, runner.getContext() );
+        RuntimeContext rt = RuntimeContext.getInstance(runner.getContext());
+        ILogger logger = (ILogger)rt.getInstanceOfType(ILogger.class);
+        if(logger!=null)
+            logger.log(m);
+        /*
         if(runner.getUncertainEngine()!=null){
             Logger logger = runner.getUncertainEngine().getLogger();        
             logger.info(m);
-        }else{
+        }
+        */
+        else{
             System.out.println(m);
         }
             
