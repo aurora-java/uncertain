0a43fae8d60062725b72a3c689b8a99cbeba717d
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2009-09-21 11:34:35 +0000

    better check

--- trunk/uncertain/uncertain/composite/decorate/ElementModifier.java
+++ trunk/uncertain/uncertain/composite/decorate/ElementModifier.java
@@ -3,6 +3,7 @@
  */
 package uncertain.composite.decorate;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -19,11 +20,11 @@ import uncertain.composite.DynamicObject;
  */
 public class ElementModifier extends DynamicObject implements ICompositeDecorator  {
     
-    public static final String KEY_ELEMENT_NAME = "elementname";
+    public static final String KEY_ELEMENT_NAME = "element";
 
-    public static final String KEY_KEYVALUE = "keyvalue";
+    public static final String KEY_KEYVALUE = "value";
 
-    public static final String KEY_KEYFIELD = "keyfield";
+    public static final String KEY_KEYFIELD = "key";
 
     public static final String KEY_ROOT_PATH = "rootpath";
     
@@ -44,9 +45,13 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
     
     static ElementModifier createInstance( String name ){
         CompositeMap map = new CompositeMap(name);
-        ElementModifier e = new ElementModifier();
-        e.initialize(map);
-        return e;
+        return createElementModifier(map);
+    }
+    
+    public static ElementModifier createElementModifier( CompositeMap config ){
+        ElementModifier m = new ElementModifier();
+        m.initialize(config);
+        return m;
     }
     
     public static ElementModifier createElementAppend(){
@@ -71,6 +76,24 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
     
     public static ElementModifier createAttributeModify(){
         return createInstance(ATTRIBUTE_MODIFY);
+    }
+    
+    public static CompositeMap process( Collection modifier_config_list, CompositeMap source ){
+        for(Iterator it=modifier_config_list.iterator(); it.hasNext(); ){
+            CompositeMap item = (CompositeMap)it.next();
+            ElementModifier em = ElementModifier.createElementModifier(item);
+            em.process(source);
+        }
+        return source;
+    }
+    
+    static String getID( CompositeMap data ){
+        String id = data.getString("Id");
+        if(id==null)
+            id=data.getString("id");
+        if(id==null)
+            id=data.getString("ID");
+        return id;
     }
     
     public String getPath(){
@@ -118,9 +141,26 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
     }
     
     public CompositeMap getTargetElement( CompositeMap root ){
+        /*
+        //First, check by id
+        String id = getID(getObjectContext());
+        if(id!=null){
+            CompositeMap child = CompositeUtil.findChild(root, null, "Id", id);
+            if(child==null)
+                throw new IllegalArgumentException("Can't find data by id:"+id+" config:"+getObjectContext().toXML());
+            return child;
+        }
+        
+        // If id not set, check 'path'
+         * 
+         */
         String path = getPath();
+        CompositeMap result = null;
         if(path!=null){
-            return (CompositeMap)root.getObject(path);
+            result = (CompositeMap)root.getObject(path); 
+            if(result==null)
+                throw new IllegalArgumentException("Can't find data from path '"+path+"', config:"+getObjectContext().toXML());
+            return result; 
         }else {
             path=getRootPath();
             String ename = getElementName();
@@ -128,10 +168,13 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
             Object keyv = getKeyValue();
             if( path!=null ){
                 if(keyf !=null && keyv != null){
-                    return CompositeUtil.findChild(root, ename, keyf, keyv==null?null:keyv.toString());
+                    result = CompositeUtil.findChild(root, ename, keyf, keyv==null?null:keyv.toString());
+                    if(result==null)
+                        throw new IllegalArgumentException("Can't find data: rootpath='"+path+"', key='"+keyf+"', value='"+keyv+"', config:"+getObjectContext().toXML());
+                    return result;
                 }
                 else
-                    throw new IllegalArgumentException("Must set 'keyField' and 'keyValue' attribute if 'rootPath' is set");
+                    throw new IllegalArgumentException("Must set 'key' and 'value' attribute if 'rootPath' is set");
             }else
                 return root;
         }
@@ -151,6 +194,8 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
             replace( source );
         else if( ATTRIBUTE_MODIFY.equalsIgnoreCase(name))
             setAttributes( source );
+        else
+            throw new IllegalArgumentException("Unknown config:"+name);
         return source;
     }
     
@@ -171,7 +216,6 @@ public class ElementModifier extends DynamicObject implements ICompositeDecorato
     
     public void removeFrom( CompositeMap source ){
         CompositeMap target = getTargetElement( source );
-        //System.out.println(target.toXML());
         if( target!=null && target != source ){
             target.getParent().removeChild(target);
         }
