0b89dadbac1fa810c273f45086385cac8a86a757
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-01-14 08:32:29 +0000

    dump a map's content

---
+++ trunk/uncertain/uncertain/proc/DumpMap.java
@@ -0,0 +1,39 @@
+/*
+ * Created on 2006-6-20
+ */
+package uncertain.proc;
+import uncertain.composite.CompositeMap;
+import uncertain.logging.ILogger;
+
+public class DumpMap extends AbstractEntry {
+    
+    public String Path;
+
+    public DumpMap() {
+        super();        
+    }
+
+    public void run(ProcedureRunner runner) {
+        String m = null;
+        Object obj = runner.getContext().getObject(Path);
+        if(obj==null)
+            m = "["+Path+"] is null";
+        else{
+            if(obj instanceof CompositeMap){
+                CompositeMap map = (CompositeMap)obj;
+                m = map.toXML();
+            }else{
+                m = "object got from ["+Path+"] is not CompositeMap but "+obj.getClass().getName()+", toString():"+obj.toString();
+            }
+        }
+        
+        ILogger logger = runner.getLogger();//LoggingContext.getLogger(runner.getContext());
+        if(logger!=null)
+            logger.log(m);        
+        else{
+            System.out.println(m);
+        }
+            
+    }
+
+}
