7e864c03d8a96f2fac09bb40cfadafa43208952f
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-03-15 04:56:23 +0000

    新增getSonElements方法，填充getRefObject方法

--- uncertain/schema/Element.java
+++ uncertain/schema/Element.java
@@ -3,140 +3,163 @@
  */
 package uncertain.schema;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import uncertain.composite.QualifiedName;
 
 public class Element extends ComplexType implements IReference {
-    
-    String          mRef;    
-    String          mUsage;
-    String          mType;
-    String          mDefault;
-    String          mMaxOccurs;
-    String          mMinOccurs;
-    
-    String          mEditor;
-    
-    boolean         mIsRef = false;
-    ComplexType     mRefType;
-    IType           mElementType;
 
-    String          mDisplayMask;
-    
-    public String getDisplayMask() {
-        return mDisplayMask;
-    }
+	String mRef;
+	String mUsage;
+	String mType;
+	String mDefault;
+	String mMaxOccurs;
+	String mMinOccurs;
 
-    public void setDisplayMask(String displayMask) {
-        this.mDisplayMask = displayMask;
-    }
-    
-    public String getRef() {
-        return mRef;
-    }
+	String mEditor;
 
-    public void setRef(String ref) {
-        this.mRef = ref;
-        mIsRef = mRef!=null;
-    }
-    
-    public ComplexType getRefType(){
-        return mRefType;
-    }
-    
-    public void doAssemble() {
-        super.doAssemble();
-    }    
-    
-    public boolean isArray(){
-        return false;
-    }
+	boolean mIsRef = false;
+	ComplexType mRefType;
+	IType mElementType;
 
-    public String getType() {
-        return mType;
-    }
+	String mDisplayMask;
 
-    public void setType(String type) {
-        mType = type;
-    }
-    
-    public IType getElementType(){
-        Schema schema = getSchema();
-        QualifiedName qname = schema.getQualifiedName(mType);
-        return getSchemaManager().getType(qname);
-    }
-    
-    public boolean isRef() {
-        return mIsRef;
-    }
-    
-    /**
-     * @return  Qualified name for referenced object
-     */
-    public QualifiedName getRefQName(){
-        return mQname;
-    }
-    
-    /**
-     * @return Referenced object instance
-     */
-    public ISchemaObject getRefObject(){
-        return null;
-    }
-    
-    public void resolveReference( ISchemaManager manager ){
-        if(mIsRef){
-            mRefType = manager.getComplexType(mQname);
-            if(mRefType==null)
-                throw new SchemaError("Unresolvable element ref:"+mRef);
-        }
-        if(mType!=null){
-            QualifiedName type_qname = getSchema().getQualifiedName(mType);
-            if(type_qname==null) 
-                throw new InvalidQNameError(mType);
-            mElementType = manager.getType(type_qname);
-        }
-        super.resolveReference(manager);
-    }
+	public String getDisplayMask() {
+		return mDisplayMask;
+	}
 
-    public void resolveQName(IQualifiedNameResolver resolver) {
-        if( mIsRef ){
-            mQname = resolver.getQualifiedName(mRef);
-            if(mQname==null)
-                throw new SchemaError("Can't resolve ref qualified name:"+mRef);
-        }else
-            super.resolveQName(resolver);
-    }
+	public void setDisplayMask(String displayMask) {
+		this.mDisplayMask = displayMask;
+	}
 
-    public String getEditor() {
-        return mEditor;
-    }
+	public String getRef() {
+		return mRef;
+	}
 
-    public void setEditor(String editor) {
-        mEditor = editor;
-    }
+	public void setRef(String ref) {
+		this.mRef = ref;
+		mIsRef = mRef != null;
+	}
 
-    public String getDefault() {
-        return mDefault;
-    }
+	public ComplexType getRefType() {
+		return mRefType;
+	}
 
-    public void setDefault(String default1) {
-        mDefault = default1;
-    }
+	public void doAssemble() {
+		super.doAssemble();
+	}
 
-    public String getMaxOccurs() {
-        return mMaxOccurs;
-    }
+	public boolean isArray() {
+		return false;
+	}
 
-    public void setMaxOccurs(String maxOccurs) {
-        mMaxOccurs = maxOccurs;
-    }
+	public String getType() {
+		return mType;
+	}
 
-    public String getMinOccurs() {
-        return mMinOccurs;
-    }
+	public void setType(String type) {
+		mType = type;
+	}
 
-    public void setMinOccurs(String minOccurs) {
-        mMinOccurs = minOccurs;
-    }
+	public IType getElementType() {
+		Schema schema = getSchema();
+		QualifiedName qname = schema.getQualifiedName(mType);
+		return getSchemaManager().getType(qname);
+	}
+
+	public boolean isRef() {
+		return mIsRef;
+	}
+
+	/**
+	 * @return Qualified name for referenced object
+	 */
+	public QualifiedName getRefQName() {
+		return mQname;
+	}
+
+	/**
+	 * @return Referenced object instance
+	 */
+	public ISchemaObject getRefObject() {
+		if (mIsRef) {
+			return mRefType;
+		} else
+			return null;
+	}
+
+	public void resolveReference(ISchemaManager manager) {
+		if (mIsRef) {
+			mRefType = manager.getComplexType(mQname);
+			if (mRefType == null)
+				throw new SchemaError("Unresolvable element ref:" + mRef);
+		}
+		if (mType != null) {
+			QualifiedName type_qname = getSchema().getQualifiedName(mType);
+			if (type_qname == null)
+				throw new InvalidQNameError(mType);
+			mElementType = manager.getType(type_qname);
+		}
+		super.resolveReference(manager);
+	}
+
+	public void resolveQName(IQualifiedNameResolver resolver) {
+		if (mIsRef) {
+			mQname = resolver.getQualifiedName(mRef);
+			if (mQname == null)
+				throw new SchemaError("Can't resolve ref qualified name:"
+						+ mRef);
+		} else
+			super.resolveQName(resolver);
+	}
+
+	public String getEditor() {
+		return mEditor;
+	}
+
+	public void setEditor(String editor) {
+		mEditor = editor;
+	}
+
+	public String getDefault() {
+		return mDefault;
+	}
+
+	public void setDefault(String default1) {
+		mDefault = default1;
+	}
+
+	public String getMaxOccurs() {
+		return mMaxOccurs;
+	}
+
+	public void setMaxOccurs(String maxOccurs) {
+		mMaxOccurs = maxOccurs;
+	}
+
+	public String getMinOccurs() {
+		return mMinOccurs;
+	}
+
+	public void setMinOccurs(String minOccurs) {
+		mMinOccurs = minOccurs;
+	}
+
+	public List getSonElements(SchemaManager manager) {
+		List sonElements = new ArrayList();
+		List sonList = getAllElements();
+		Iterator sonIterator = sonList.iterator();
+		while (sonIterator.hasNext()) {
+			Object ele = sonIterator.next();
+			ComplexType ele_ct = (ComplexType) ele;
+			if (ele_ct instanceof Element) {
+				sonElements.add(ele_ct);
+			} else
+				sonElements.addAll(manager.getElementsOfType(ele_ct));
+		}
+		return sonElements;
+	}
 
 }
