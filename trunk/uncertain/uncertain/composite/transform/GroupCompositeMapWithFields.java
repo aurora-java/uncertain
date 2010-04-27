af47d8cf19ceab2a951f99def0f42d5e3ac293ba
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-04-27 03:58:35 +0000

    添加excel分组功能

---
+++ trunk/uncertain/uncertain/composite/transform/GroupCompositeMapWithFields.java
@@ -0,0 +1,143 @@
+package uncertain.composite.transform;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.json.Test;
+
+import uncertain.composite.CompositeMap;
+
+public class GroupCompositeMapWithFields {
+	private static GroupCompositeMapWithFields default_instance;
+	
+	private GroupCompositeMapWithFields(){
+	}
+	public static GroupCompositeMapWithFields getInstance(){
+		if(default_instance == null)
+			default_instance = new GroupCompositeMapWithFields();
+		return default_instance;
+	}
+	
+	
+	public List groupCompositeMap(CompositeMap data, String[] groupfields) {
+		if (data == null || groupfields == null
+				|| data.getChildsNotNull().size() < 2) {
+			return null;
+		}
+		Iterator childs = data.getChildsNotNull().iterator();
+		CompositeMap preRecord = null;
+		CompositeMap nextRecord = null;
+		int rowNum = 0;
+		BreakPoints bp = new BreakPoints(data.getChildsNotNull().size(),
+				groupfields.length);
+		while (childs.hasNext()) {
+			CompositeMap nowRecord = (CompositeMap) childs.next();
+			if (preRecord != null) {
+				boolean breakbegin = false;
+				for (int y = 0; y < groupfields.length; y++) {
+					if (breakbegin) {
+						bp.addPoint(rowNum, y);
+						continue;
+					}
+					String key = groupfields[y];
+					Object nowObject = nowRecord.get(key);
+					Object preObject = preRecord.get(key);
+					if(nowObject ==null){
+						if(preObject ==null)continue;
+						else breakbegin = true;
+					}
+					else if (!nowRecord.get(key).equals(preRecord.get(key))){
+						breakbegin = true;
+					}
+					if (breakbegin) {
+						bp.addPoint(rowNum, y);
+						System.out.println("rowNum:" + rowNum + " column:" + y);
+//						breakbegin = true;
+					}
+				}
+			}
+			preRecord = nowRecord;
+			rowNum++;
+		}
+
+		return bp.getMergeRange();
+	}
+
+	class BreakPoints {
+		int[][] matric;
+		int[] columnIndex;
+
+		BreakPoints(int rowCount, int columnCount) {
+			System.out.println("rowcount:" + rowCount + " columnCount:"
+					+ columnCount);
+			matric = new int[columnCount][rowCount];
+			columnIndex = new int[columnCount];
+		}
+
+		void addPoint(int row, int column) {
+			// System.out.println("row:"+row+" column:"+column);
+			// System.out.println("columnIndex[column]"+columnIndex[column]);
+			matric[column][columnIndex[column]] = row;
+			columnIndex[column]++;
+		}
+
+		List getMergeRange() {
+			LinkedList list = new LinkedList();
+			int[] range = new int[4];
+			for (int x = 0; x < matric.length; x++) {
+				int preValue = 0;
+				for (int y = 0; y < matric[x].length; y++) {
+					int value = matric[x][y];
+					// System.out.print(value+",");
+					if (value == 0){
+						if(matric[x].length-preValue>2){
+							list.add(new int[] { preValue, x, matric[x].length-1, x });
+						}
+						break;
+					}	
+					if (value - preValue > 1) {
+						list.add(new int[] { preValue, x, value - 1, x });
+					}
+					preValue = value;
+				}
+				System.out.println();
+			}
+			return list;
+		}
+	}
+
+	public static void main(String[] args) {
+		CompositeMap target = new CompositeMap();
+		String[][] rows = new String[][] {
+				{ "a1", "a2", "a3", "a4", "a5", "a6" },
+				{ "a1", "a2", "a4", "a5", "a7", "a9" },
+				{ "a2", "a3", "a4", "a9", "a2", "a6" },
+				{ "a3", "a2", "a4", "a6", "a3", "a7" },
+				{ "a3", "a8", "a1", "a4", "a4", "a8" },
+				{ "a3", "a7", "a2", "a3", "a5", "a9" } };
+		String[] allFields = new String[] { "B1", "B2", "B3", "B4", "B5", "B6" };
+		for (int x = 0; x < rows.length; x++) {
+			CompositeMap child = new CompositeMap();
+			for (int y = 0; y < rows[x].length; y++) {
+				child.put(allFields[y], rows[x][y]);
+			}
+			target.addChild(child);
+		}
+
+		String[] groupfields = new String[] { "B1", "B2", "B3" };
+		GroupCompositeMapWithFields test = new GroupCompositeMapWithFields();
+		System.out.println(target.toXML());
+		List mergeRange = test.groupCompositeMap(target, groupfields);
+
+		Iterator it = mergeRange.iterator();
+		while (it.hasNext()) {
+			System.out.println();
+			int[] range = (int[]) it.next();
+			for (int i = 0; i < range.length; i++) {
+				System.out.print(range[i] + ",");
+			}
+		}
+
+	}
+}
