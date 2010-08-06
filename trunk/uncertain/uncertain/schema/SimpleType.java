5d7375e842e9495715e4d1317a58b87699a36519
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-08-06 02:37:14 +0000

    添加Restriction子节点

--- trunk/uncertain/uncertain/schema/SimpleType.java
+++ trunk/uncertain/uncertain/schema/SimpleType.java
@@ -6,12 +6,25 @@ package uncertain.schema;
 
 public class SimpleType extends AbstractQualifiedNamed implements IType {
   
+    Restriction     mRestriction;
+    public Restriction getRestriction() {
+		return mRestriction;
+	}
+
+	public void addRestriction(Restriction restriction) {
+		this.mRestriction = restriction;
+		addChild(this.mRestriction);
+	}
     public boolean isComplex() {
         return false;
     }
     
     public void doAssemble(){
         
+    }
+    public void resolveQName(IQualifiedNameResolver resolver) {
+        super.resolveQName(resolver);
+
     }
     
     public boolean isExtensionOf( IType another ){
