93e22888ae626c4fc640241508b1476c42bde8fb
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-08-06 02:32:26 +0000

    添加了editor属性

--- trunk/uncertain/uncertain/schema/Attribute.java
+++ trunk/uncertain/uncertain/schema/Attribute.java
@@ -5,149 +5,175 @@ package uncertain.schema;
 
 import uncertain.composite.QualifiedName;
 
-
 public class Attribute extends AbstractCategorized implements IReference {
-    
-    boolean         mIsRef = false;
-    String          mRef;    
-    Attribute       mRefAttribute;
-    
-    String          mType;    
-    String          mUse;
-    String          mDefault;
-    String          mEditor;    
-    String          mDocument;
-    
-    
-    IValidator[]    mValidators;
 
+	boolean mIsRef = false;
+	String mRef;
+	Attribute mRefAttribute;
 
-    public static Attribute createInstance( String name ){
-        QualifiedName qname = new QualifiedName(null, null, name);
-        Attribute attrib = new Attribute();
-        attrib.setQName(qname);
-        return attrib;
-    }
-    
-    public Attribute(){
-        super();
-    }
+	String mType;
+	String mUse;
+	String mDefault;
+	String mEditor;
+	String mDocument;
+	QualifiedName mEditorQName;
 
-    public String getRef() {
-        return mRef;
-    }
+	IValidator[] mValidators;
 
-    public void setRef(String ref) {
-        this.mRef = ref;
-        mIsRef = mRef!=null;
-    }
+	public static Attribute createInstance(String name) {
+		QualifiedName qname = new QualifiedName(null, null, name);
+		Attribute attrib = new Attribute();
+		attrib.setQName(qname);
+		return attrib;
+	}
 
-    public IValidator[] getValidators() {
-        if( mIsRef)
-            return mRefAttribute.mValidators;
-        else
-            return mValidators;
-    }
+	public Attribute() {
+		super();
+	}
 
-    public void setValidators(IValidator[] validators) {
-        this.mValidators = validators;
-    }
+	public String getRef() {
+		return mRef;
+	}
 
-    public String getType() {
-        return mType;
-    }
+	public void setRef(String ref) {
+		this.mRef = ref;
+		mIsRef = mRef != null;
+	}
 
-    public void setType(String type) {
-        this.mType = type;
-    }
+	public IValidator[] getValidators() {
+		if (mIsRef)
+			return mRefAttribute.mValidators;
+		else
+			return mValidators;
+	}
 
-    public String getUse() {
-        return mUse;
-    }
+	public void setValidators(IValidator[] validators) {
+		this.mValidators = validators;
+	}
 
-    public void setUse(String use) {
-        this.mUse = use;
-    }
-    
-    public String getDocument() {
-        return mDocument;
-    }
+	public String getType() {
+		return mType;
+	}
 
-    public void setDocument(String document) {
-        this.mDocument = document;
-    }
-    
-    public String toString(){
-        if( mRefAttribute!=null)
-            return mRefAttribute.toString();
-        if(mQname==null)
-            return "attribute";
-        else
-            return mQname.toString();
-    }
-    
-    public boolean isRef(){
-        return mIsRef;
-    }
-    
-    public QualifiedName getRefQName(){
-        return mQname;
-    }
+	public void setType(String type) {
+		this.mType = type;
+	}
 
-    public ISchemaObject getRefObject(){
-        return getRefAttribute();
-    }
-    
-    public Attribute getRefAttribute(){
-        if(mIsRef){ 
-            return mRefAttribute;
-        }
-        else
-            return null;
-    }
-    
-    public boolean equals( Object another ){
-        return this == another;
-    }
+	public String getUse() {
+		return mUse;
+	}
 
-    public String getDefault() {
-        return mDefault;
-    }
+	public void setUse(String use) {
+		this.mUse = use;
+	}
 
-    public void setDefault(String default1) {
-        mDefault = default1;
-    }
+	public String getDocument() {
+		return mDocument;
+	}
 
-    
-    public void resolveReference( ISchemaManager manager ){ 
-        super.resolveReference(manager);
-        if(mIsRef){
-            Schema schema = getSchema();
-            QualifiedName qname = schema.getQualifiedName(mRef);
-            if(qname==null) 
-                throw new SchemaError("Can't resolve qualified name:"+mRef);
-            this.mQname = qname;
-            mRefAttribute = manager.getAttribute(qname);
-            if(mRefAttribute==null)
-                throw new SchemaError("Unresolvable attribute ref:"+mRef);
-        }         
-    }    
-    
-    public void resolveQName(IQualifiedNameResolver resolver) {   
-        super.resolveQName(resolver);
-        if( mIsRef ){
-            mQname = resolver.getQualifiedName(mRef);
-            if(mQname==null)
-                throw new SchemaError("Can't resolve ref qualified name:"+mRef);
-        }
-    }
+	public void setDocument(String document) {
+		this.mDocument = document;
+	}
 
-    public String getEditor() {
-        return mEditor;
-    }
+	public String toString() {
+		if (mRefAttribute != null)
+			return mRefAttribute.toString();
+		if (mQname == null)
+			return "attribute";
+		else
+			return mQname.toString();
+	}
 
-    public void setEditor(String editor) {
-        mEditor = editor;
-    }
-   
+	public boolean isRef() {
+		return mIsRef;
+	}
+
+	public QualifiedName getRefQName() {
+		return mQname;
+	}
+
+	public ISchemaObject getRefObject() {
+		return getRefAttribute();
+	}
+
+	public Attribute getRefAttribute() {
+		if (mIsRef) {
+			return mRefAttribute;
+		} else
+			return null;
+	}
+
+	public boolean equals(Object another) {
+		return this == another;
+	}
+
+	public String getDefault() {
+		return mDefault;
+	}
+
+	public void setDefault(String default1) {
+		mDefault = default1;
+	}
+
+	public void resolveReference(ISchemaManager manager) {
+		super.resolveReference(manager);
+		if (mIsRef) {
+			Schema schema = getSchema();
+			QualifiedName qname = schema.getQualifiedName(mRef);
+			if (qname == null)
+				throw new SchemaError("Can't resolve qualified name:" + mRef);
+			this.mQname = qname;
+			mRefAttribute = manager.getAttribute(qname);
+			if (mRefAttribute == null)
+				throw new SchemaError("Unresolvable attribute ref:" + mRef);
+		}
+	}
+
+	public void resolveQName(IQualifiedNameResolver resolver) {
+		super.resolveQName(resolver);
+		if (mIsRef) {
+			mQname = resolver.getQualifiedName(mRef);
+			if (mQname == null)
+				throw new SchemaError("Can't resolve ref qualified name:"
+						+ mRef);
+		}
+		if (mEditor != null && !mEditor.equals("")) {
+			mEditorQName = resolver.getQualifiedName(mEditor);
+			if (mEditorQName == null)
+				throw new SchemaError("Can't resolve editor qualified name:"
+						+ mEditor);
+		}
+	}
+
+	public String getEditor() {
+		return mEditor;
+	}
+
+	public void setEditor(String editor) {
+		mEditor = editor;
+	}
+
+	public QualifiedName getEditorQName() {
+		return mEditorQName;
+	}
+
+	public void setEditorQName(QualifiedName editorQName) {
+		this.mEditorQName = editorQName;
+	}
+
+	public IType getAttributeType() {
+		Schema schema = getSchema();
+		QualifiedName qname = null;
+		if (mType != null)
+			qname = schema.getQualifiedName(mType);
+		return getSchemaManager().getType(qname);
+	}
+
+	public QualifiedName getTypeQName() {
+		Schema schema = getSchema();
+		QualifiedName qname = null;
+		if (mType != null)
+			qname = schema.getQualifiedName(mType);
+		return qname;
+	}
 }
