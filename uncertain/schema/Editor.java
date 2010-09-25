0ec5cfcef3022ff96e4bde5e95724033b301836d
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-09-25 05:32:57 +0000

    去掉type相关属性

--- uncertain/schema/Editor.java
+++ uncertain/schema/Editor.java
@@ -1,12 +1,9 @@
 package uncertain.schema;
 
-import uncertain.composite.QualifiedName;
 
 public class Editor extends AbstractQualifiedNamed implements IType {
 
 	String mInstanceClass;
-	String mType;
-	IType mElementType;
 
 	public String getInstanceClass() {
 		return mInstanceClass;
@@ -24,27 +21,7 @@ public class Editor extends AbstractQualifiedNamed implements IType {
 		return false;
 	}
 
-	public String getType() {
-		return mType;
-	}
-
-	public void setType(String type) {
-		mType = type;
-	}
-
-	public IType getElementType() {
-		Schema schema = getSchema();
-		QualifiedName qname = schema.getQualifiedName(mType);
-		return getSchemaManager().getType(qname);
-	}
-
 	public void resolveQName(IQualifiedNameResolver resolver) {
-		if (mType != null) {
-			QualifiedName type_qname = getSchema().getQualifiedName(mType);
-			if (type_qname == null)
-				throw new InvalidQNameError(mType);
-			mElementType = getSchemaManager().getType(type_qname);
-		}
 		super.resolveQName(resolver);
 	}
 
