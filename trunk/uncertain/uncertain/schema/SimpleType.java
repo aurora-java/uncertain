160a88ebe850a8c5d1b146187ddd293ad608c45b
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-09-25 05:31:57 +0000

    修改editor的关联次序： 以前： attribute上有editor属性 现在： attribute上有type属性，type有editor属性

--- trunk/uncertain/uncertain/schema/SimpleType.java
+++ trunk/uncertain/uncertain/schema/SimpleType.java
@@ -3,10 +3,14 @@
  */
 package uncertain.schema;
 
+import uncertain.composite.QualifiedName;
+
 
 public class SimpleType extends AbstractQualifiedNamed implements IType {
   
     Restriction     mRestriction;
+	String mEditor;
+	QualifiedName mEditorQName;
     public Restriction getRestriction() {
 		return mRestriction;
 	}
@@ -24,9 +28,29 @@ public class SimpleType extends AbstractQualifiedNamed implements IType {
     }
     public void resolveQName(IQualifiedNameResolver resolver) {
         super.resolveQName(resolver);
+		if (mEditor != null && !mEditor.equals("")) {
+			mEditorQName = resolver.getQualifiedName(mEditor);
+			if (mEditorQName == null)
+				throw new SchemaError("Can't resolve editor qualified name:"
+						+ mEditor);
+		}
 
     }
-    
+	public String getEditor() {
+		return mEditor;
+	}
+
+	public void setEditor(String editor) {
+		mEditor = editor;
+	}
+
+	public QualifiedName getEditorQName() {
+		return mEditorQName;
+	}
+
+	public void setEditorQName(QualifiedName editorQName) {
+		this.mEditorQName = editorQName;
+	}
     public boolean isExtensionOf( IType another ){
         return false;    
     }    
