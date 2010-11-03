85878ac8c3b9f979c02aab4eb4c559742bd11125
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-03 07:58:23 +0000

    添加editor和wizard类型

--- trunk/uncertain/uncertain/schema/SchemaConstant.java
+++ trunk/uncertain/uncertain/schema/SchemaConstant.java
@@ -12,6 +12,7 @@ public final class SchemaConstant {
     public static final int TYPE_ATTRIBUTE = 1;
     public static final int TYPE_CATEGORIE = 2;
     public static final int TYPE_EDITOR = 3;
+    public static final int TYPE_WIZARD = 4;
     
     public static final String USAGE_REQUIRED = "required";
     public static final String USAGE_OPTIONAL = "optional";
