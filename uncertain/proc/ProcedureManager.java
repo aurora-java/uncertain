d6b73c7b4b4eb50185f2e0bfe8342f3e374fdae8
seacat.zhou@gmail.com <seacat@users.noreply.github.com>
2011-04-19 05:41:41 +0000

    Now supports cache

--- uncertain/proc/ProcedureManager.java
+++ uncertain/proc/ProcedureManager.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 
 import org.xml.sax.SAXException;
 
+import uncertain.cache.ICache;
+import uncertain.cache.MapBasedCache;
 import uncertain.composite.CompositeLoader;
 import uncertain.composite.CompositeMap;
 import uncertain.core.UncertainEngine;
@@ -38,14 +40,53 @@ public class ProcedureManager implements IProcedureManager {
         mUncertainEngine = uncertainEngine;
         mOCManager = mUncertainEngine.getOcManager();
         mCompositeLoader = CompositeLoader.createInstanceForOCM(DEFAULT_PROC_EXTENSION);
-        //mCompositeLoader.setCacheEnabled(true);
+//        setIsCache(uncertainEngine.getCacheConfigFiles());
     }
-
+    
     public static final String DEFAULT_PROC_EXTENSION = "proc";
     
     UncertainEngine mUncertainEngine;
     OCManager       mOCManager;
     CompositeLoader mCompositeLoader;
+    boolean         mIsCache = false;
+    ICache          mCache;
+    
+    private void createCache(){
+        if(mCache!=null){
+            mCache.clear();
+            mCache=null;
+        }
+        if(mUncertainEngine!=null){
+            mCache = mUncertainEngine.createNamedCache("ProcedureConfig");
+        }else{
+            mCache = new MapBasedCache();
+        }
+    }
+
+    public boolean getIsCache() {
+        return mIsCache;
+    }
+
+    public void setIsCache(boolean isCache) {
+        mIsCache = isCache;
+        if(mIsCache)
+            createCache();
+        else{
+            if(mCache!=null){
+                mCache.clear();
+                mCache = null;
+            }
+        }
+        
+    }
+
+    public ICache getCache() {
+        return mCache;
+    }
+
+    public void setCache(ICache cache) {
+        this.mCache = cache;
+    }
 
     public Procedure createProcedure() {
         Procedure proc = new Procedure();
@@ -62,13 +103,26 @@ public class ProcedureManager implements IProcedureManager {
         return runner;
     }
 
-    public Procedure loadProcedure(String name) 
+    private Procedure loadProcedureNC(String name) 
         throws IOException, SAXException
     {
         CompositeMap map = mCompositeLoader.loadFromClassPath(name);
         if(map==null)
             throw new IOException("Can't load config file "+name);
         return createProcedure(map);
+    }
+    
+    public Procedure loadProcedure(String name) 
+    throws IOException, SAXException
+    {
+        if(!mIsCache || mCache==null)
+            return loadProcedureNC(name);
+        Procedure proc = (Procedure)mCache.getValue(name);
+        if(proc==null){
+            proc = loadProcedureNC(name);
+            mCache.setValue(name, proc);
+        }
+        return proc;
     }
     
     public Configuration createConfig(){
