3520b5c92b7653b63ee69050afcf8051177dedb9
seacat.zhou@gmail.com <seacat@users.noreply.github.com>
2011-09-04 07:39:38 +0000

    add property case

--- uncertain/util/MapHandle.java
+++ uncertain/util/MapHandle.java
@@ -12,15 +12,33 @@ import java.util.Map;
 public class MapHandle implements TagParseHandle {
 	
 	Map		params;
+	byte    attribCase = 0;
 	
 	public MapHandle(Map params){
 		this.params = params;
 	}
+	/**
+	 * 
+	 * @param params
+	 * @param attrib_case 0: nochange 1:upper 2:lower
+	 */
+	public MapHandle(Map params, byte attrib_case){
+        this.params = params;
+        this.attribCase = attrib_case;
+    }
 	
 	/**
 	 * @see uncertain.util.TagParseHandle#ProcessTag(int, String)
 	 */
 	public String ProcessTag(int index, String tag) {
+	    switch(attribCase){
+    	    case Character.UPPERCASE_LETTER:
+    	        tag = tag.toUpperCase();
+    	        break;
+    	    case Character.LOWERCASE_LETTER:
+    	        tag = tag.toLowerCase();
+    	        break;
+	    }
 		Object obj = params.get(tag);
 		if(obj == null) return "";
 		else return obj.toString();
