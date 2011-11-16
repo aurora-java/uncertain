cbb07ae1253353cde5faeabca86b4324f0a0a8d8
seacat.zhou@gmail.com <seacat@users.noreply.github.com>
2011-11-16 07:15:48 +0000

    move getSortedList() from UncertainEngine to FileUtil

--- uncertain/util/FileUtil.java
+++ uncertain/util/FileUtil.java
@@ -7,6 +7,10 @@ package uncertain.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class FileUtil {
 
@@ -39,6 +43,25 @@ public class FileUtil {
     public static void deleteDirectory(String dir_to_del) throws IOException {
         File file = new File(dir_to_del);
         deleteDirectory(file);
+    }
+
+    public static List<File> getSortedList(File[] files) {
+        List<File> lst = new LinkedList<File>();
+        for (int i = 0; i < files.length; i++)
+            lst.add(files[i]);
+        Collections.sort(lst, new Comparator() {
+    
+            public int compare(Object o1, Object o2) {
+                return ((File) o1).getAbsolutePath().compareTo(
+                        ((File) o2).getAbsolutePath());
+            }
+    
+            public boolean equals(Object obj) {
+                return obj == this;
+            }
+    
+        });
+        return lst;
     }
 
 
