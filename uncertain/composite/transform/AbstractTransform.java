63feb472b84843edce04cd0524da10c4b971e7c6
seacat.zhou@gmail.com <seacat@users.noreply.github.com>
2012-02-10 04:22:48 +0000

    Abstract parent class for transformers

---
+++ uncertain/composite/transform/AbstractTransform.java
@@ -0,0 +1,13 @@
+/*
+ * Created on 2011-11-21 下午10:40:29
+ * $Id$
+ */
+package uncertain.composite.transform;
+
+import uncertain.proc.AbstractEntry;
+import uncertain.proc.ProcedureRunner;
+
+public abstract class AbstractTransform extends AbstractEntry {
+
+
+}
